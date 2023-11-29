d9edcd5a41ac3be39d898617f914a336ae9cba38
Arnau Mora <arnyminerz@proton.me>
2023-11-29 17:53:54 +0100

    Fixed detekt issues
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- server/src/test/kotlin/Utils.kt
+++ server/src/test/kotlin/Utils.kt
@@ -3,7 +3,7 @@ import org.junit.Assert
 
 fun expectToBlock(thread: Thread, waitCount: Long, waitUnits: TimeUnit) {
     val start = System.currentTimeMillis()
-    while(System.currentTimeMillis() - start < waitUnits.toMillis(waitCount)) {
+    while (System.currentTimeMillis() - start < waitUnits.toMillis(waitCount)) {
         if (thread.state == Thread.State.WAITING) {
             return
         }
