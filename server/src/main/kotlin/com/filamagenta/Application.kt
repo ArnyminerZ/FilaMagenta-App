05841b38f291d5490a697ddfbbdca9d3b3616579
Arnau Mora <arnyminerz@proton.me>
2023-11-29 13:56:18 +0100

    Project set up
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

---
+++ server/src/main/kotlin/com/filamagenta/Application.kt
@@ -0,0 +1,31 @@
+package com.filamagenta
+
+import SERVER_PORT
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+
+fun main() {
+    embeddedServer(
+        Netty,
+        port = SERVER_PORT,
+        host = "0.0.0.0",
+        module = Application::module,
+        watchPaths = listOf(
+            "classes",
+            "resources"
+        )
+    ).start(wait = true)
+}
+
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Welcome!")
+        }
+    }
+}
