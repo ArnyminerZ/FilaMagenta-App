6b06fb6ae64c92149f722538ef208ffc47ccc6fb
Arnau Mora <arnyminerz@proton.me>
2023-11-29 08:09:57 -0800

    Added more endpoint operations (#1)
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- server/src/main/kotlin/com/filamagenta/endpoint/EndpointLogic.kt
+++ server/src/main/kotlin/com/filamagenta/endpoint/EndpointLogic.kt
@@ -1,7 +1,10 @@
 package com.filamagenta.endpoint
 
 import io.ktor.server.routing.Routing
+import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
+import io.ktor.server.routing.patch
+import io.ktor.server.routing.post
 
 /**
  * Adds a GET receiver for the given [endpoint].
@@ -12,4 +15,37 @@ import io.ktor.server.routing.get
  */
 fun Routing.get(endpoint: Endpoint) = with(endpoint) {
     get(url) { body() }
+}
+
+/**
+ * Adds a POST receiver for the given [endpoint].
+ *
+ * @param endpoint The endpoint to add.
+ *
+ * @return The route created.
+ */
+fun Routing.post(endpoint: Endpoint) = with(endpoint) {
+    post(url) { body() }
+}
+
+/**
+ * Adds a GET receiver for the given [endpoint].
+ *
+ * @param endpoint The endpoint to add.
+ *
+ * @return The route created.
+ */
+fun Routing.patch(endpoint: Endpoint) = with(endpoint) {
+    patch(url) { body() }
+}
+
+/**
+ * Adds a DELETE receiver for the given [endpoint].
+ *
+ * @param endpoint The endpoint to add.
+ *
+ * @return The route created.
+ */
+fun Routing.delete(endpoint: Endpoint) = with(endpoint) {
+    delete(url) { body() }
 }
