8f238f0719129eb897e75a92cc100f5f8ea524c6
Arnau Mora <arnyminerz@proton.me>
2023-11-29 19:47:54 +0100

    Added database disposal
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- server/src/test/kotlin/database/DatabaseTestEnvironment.kt
+++ server/src/test/kotlin/database/DatabaseTestEnvironment.kt
@@ -3,6 +3,7 @@ package database
 import com.filamagenta.database.Database
 import com.filamagenta.system.EnvironmentVariables
 import database.stub.TestTable
+import org.junit.After
 import org.junit.Before
 
 abstract class DatabaseTestEnvironment {
@@ -12,5 +13,13 @@ abstract class DatabaseTestEnvironment {
         EnvironmentVariables.Database.Driver._value = "org.h2.Driver"
 
         Database.initialize(TestTable)
+    }
+
+    @After
+    fun disposeEnvironment() {
+        EnvironmentVariables.Database.Url.dispose()
+        EnvironmentVariables.Database.Driver.dispose()
+
+        Database.instance = null
     }
 }
