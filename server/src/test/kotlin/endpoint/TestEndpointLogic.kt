24408376fdfddbf18bfbfa4e70840f3a03457721
Arnau Mora <arnyminerz@proton.me>
2023-11-29 16:32:46 +0100

    Added more endpoint operations
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- server/src/test/kotlin/endpoint/TestEndpointLogic.kt
+++ server/src/test/kotlin/endpoint/TestEndpointLogic.kt
@@ -1,21 +1,32 @@
 package endpoint
 
 import com.filamagenta.endpoint.Endpoint
+import com.filamagenta.endpoint.delete
 import com.filamagenta.endpoint.get
+import com.filamagenta.endpoint.patch
+import com.filamagenta.endpoint.post
+import io.ktor.client.request.delete
 import io.ktor.client.request.get
+import io.ktor.client.request.patch
+import io.ktor.client.request.post
+import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.application.call
 import io.ktor.server.response.respondText
+import io.ktor.server.routing.Routing
+import io.ktor.server.testing.ApplicationTestBuilder
 import io.ktor.server.testing.testApplication
 import io.ktor.util.pipeline.PipelineContext
 import kotlin.test.assertEquals
 import org.junit.Test
 
 class TestEndpointLogic {
-    @Test
-    fun `test creating GET endpoint`() = testApplication {
+    private fun testEndpoint(
+        adder: Routing.(endpoint: Endpoint) -> Unit,
+        operation: suspend ApplicationTestBuilder.(String) -> HttpResponse
+    ) = testApplication {
         val endpoint = object : Endpoint("test") {
             override suspend fun PipelineContext<Unit, ApplicationCall>.body() {
                 call.respondText("ok")
@@ -23,12 +34,36 @@ class TestEndpointLogic {
         }
 
         routing {
-            get(endpoint)
+            adder(endpoint)
         }
 
-        client.get("test").let { response ->
+        operation("test").let { response ->
             assertEquals(HttpStatusCode.OK, response.status)
             assertEquals("ok", response.bodyAsText())
         }
     }
+
+    @Test
+    fun `test creating GET endpoint`() = testEndpoint(
+        { get(it) },
+        { client.get(it) }
+    )
+
+    @Test
+    fun `test creating POST endpoint`() = testEndpoint(
+        { post(it) },
+        { client.post(it) }
+    )
+
+    @Test
+    fun `test creating PATCH endpoint`() = testEndpoint(
+        { patch(it) },
+        { client.patch(it) }
+    )
+
+    @Test
+    fun `test creating DELETE endpoint`() = testEndpoint(
+        { delete(it) },
+        { client.delete(it) }
+    )
 }
