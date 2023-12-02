bb7df76ed68568ca4cfd587c72c4c9df2f62bd94
Arnau Mora <arnyminerz@proton.me>
2023-12-02 11:25:59 +0100

    Added test for roles
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

---
+++ server/src/test/kotlin/database/table/UserRolesTest.kt
@@ -0,0 +1,51 @@
+package database.table
+
+import com.filamagenta.database.Database
+import com.filamagenta.database.entity.UserRole
+import com.filamagenta.security.Roles
+import database.model.DatabaseTestEnvironment
+import database.provider.UserProvider
+import kotlin.test.assertEquals
+import org.jetbrains.exposed.exceptions.ExposedSQLException
+import org.junit.Assert.assertThrows
+import org.junit.Test
+
+class UserRolesTest : DatabaseTestEnvironment() {
+    @Test
+    fun `test creation`() {
+        val entry = Database.transaction {
+            val user = userProvider.createSampleUser()
+
+            UserRole.new {
+                this.role = Roles.Users.ModifyOthers
+                this.user = user
+            }
+        }
+        Database.transaction {
+            UserRole[entry.id].let {
+                assertEquals(Roles.Users.ModifyOthers, it.role)
+                assertEquals(UserProvider.SampleUser.NIF, it.user.nif)
+            }
+        }
+    }
+
+    @Test
+    fun `test duplicates not allowed`() {
+        val user = Database.transaction { userProvider.createSampleUser() }
+
+        Database.transaction {
+            UserRole.new {
+                this.role = Roles.Users.ModifyOthers
+                this.user = user
+            }
+        }
+        assertThrows(ExposedSQLException::class.java) {
+            Database.transaction {
+                UserRole.new {
+                    this.role = Roles.Users.ModifyOthers
+                    this.user = user
+                }
+            }
+        }
+    }
+}
