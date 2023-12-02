707a41f5c4e23c77b75f69d0c9d5ba1ded75ca98
Arnau Mora <arnyminerz@proton.me>
2023-12-02 10:43:56 +0100

    Added roles verification
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- server/src/main/kotlin/com/filamagenta/endpoint/model/SecureEndpoint.kt
+++ server/src/main/kotlin/com/filamagenta/endpoint/model/SecureEndpoint.kt
@@ -2,24 +2,54 @@ package com.filamagenta.endpoint.model
 
 import com.filamagenta.database.Database
 import com.filamagenta.database.entity.User
+import com.filamagenta.database.entity.UserRole
+import com.filamagenta.database.table.UserRolesTable
 import com.filamagenta.database.table.Users
 import com.filamagenta.modules.AUTH_JWT_CLAIM_NIF
 import com.filamagenta.response.Errors
+import com.filamagenta.security.Role
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.application.call
 import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.principal
 import io.ktor.util.pipeline.PipelineContext
 
-abstract class SecureEndpoint(url: String) : Endpoint(url) {
+/**
+ * Represents an endpoint in the server which requires the user to be authenticated.
+ *
+ * @param url The url of the endpoint in the server.
+ * @param roles If any, the user that makes a request to this endpoint must have these roles.
+ */
+abstract class SecureEndpoint(
+    url: String,
+    vararg roles: Role
+) : Endpoint(url) {
+    private val roles = roles.toList()
+
     override suspend fun PipelineContext<Unit, ApplicationCall>.body() {
         try {
             val principal = call.principal<JWTPrincipal>()
             val nif = principal!!.payload.getClaim(AUTH_JWT_CLAIM_NIF).asString()
 
+            // Make sure the user exists
             val user = Database.transaction { User.find { Users.nif eq nif }.firstOrNull() }
                 ?: return respondFailure(Errors.Authentication.JWT.UserNotFound)
 
+            if (roles.isNotEmpty()) {
+                // Fetch all the user's roles
+                val roles = Database.transaction {
+                    UserRole.find { UserRolesTable.user eq user.id }
+                        .map { it.role }
+                }
+                // Make sure that the user has all of them
+                for (role in this@SecureEndpoint.roles) {
+                    if (!roles.contains(role)) {
+                        return respondFailure(Errors.Authentication.JWT.MissingRole)
+                    }
+                }
+            }
+
+            // If all requirements have been met, call the secure body
             secureBody(user)
         } catch (_: NullPointerException) {
             respondFailure(Errors.Authentication.JWT.MissingData)
