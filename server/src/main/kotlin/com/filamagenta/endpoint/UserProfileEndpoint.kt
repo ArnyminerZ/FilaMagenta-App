9a0b415831795e9455e8a678274a140a508e48f6
Arnau Mora <arnyminerz@proton.me>
2023-12-02 12:31:39 +0100

    Profile now displays roles (#19)
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- server/src/main/kotlin/com/filamagenta/endpoint/UserProfileEndpoint.kt
+++ server/src/main/kotlin/com/filamagenta/endpoint/UserProfileEndpoint.kt
@@ -4,9 +4,12 @@ import KoverIgnore
 import com.filamagenta.database.Database
 import com.filamagenta.database.entity.User
 import com.filamagenta.database.entity.UserMeta
+import com.filamagenta.database.entity.UserRole
 import com.filamagenta.database.table.UserMetaTable
+import com.filamagenta.database.table.UserRolesTable
 import com.filamagenta.endpoint.model.SecureEndpoint
 import com.filamagenta.endpoint.model.respondSuccess
+import com.filamagenta.security.Role
 import io.ktor.server.application.ApplicationCall
 import io.ktor.util.pipeline.PipelineContext
 import kotlinx.serialization.Serializable
@@ -18,7 +21,8 @@ object UserProfileEndpoint : SecureEndpoint("/user/profile") {
         val nif: String,
         val name: String,
         val surname: String,
-        val meta: Map<UserMeta.Key, String>
+        val meta: Map<UserMeta.Key, String>,
+        val roles: List<Role>
     )
 
     override suspend fun PipelineContext<Unit, ApplicationCall>.secureBody(user: User) {
@@ -26,13 +30,17 @@ object UserProfileEndpoint : SecureEndpoint("/user/profile") {
             UserMeta.find { UserMetaTable.user eq user.id }
                 .associate { it.key to it.value }
         }
+        val roles = Database.transaction {
+            UserRole.find { UserRolesTable.user eq user.id }.map { it.role }
+        }
 
         respondSuccess(
             UserProfileResponse(
                 user.nif,
                 user.name,
                 user.surname,
-                meta
+                meta,
+                roles
             )
         )
     }
