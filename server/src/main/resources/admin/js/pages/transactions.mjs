ef067804153d7a11820b5122b5916565c26e0324
Arnau Mora <arnyminerz@proton.me>
2023-12-10 17:23:46 +0100

    Added transactions list
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- server/src/main/resources/admin/js/pages/transactions.mjs
+++ server/src/main/resources/admin/js/pages/transactions.mjs
@@ -3,6 +3,24 @@ import {STORAGE_PROFILE, STORAGE_TOKEN} from "../const.js";
 import {_} from "../utils.mjs";
 import {get} from "../request.js";
 
+/**
+ * @typedef {Object} Transaction
+ * @property {string} date
+ * @property {string} description
+ * @property {boolean} income
+ * @property {number} units
+ * @property {number} pricePerUnit
+ * @property {Object} type
+ * @property {string} type.type
+ */
+
+/**
+ * @typedef {APIResult} TransactionsListResult
+ * @property {boolean} success
+ * @property {Object} data
+ * @property {Transaction[]} data.transactions
+ */
+
 window.addEventListener('load', async function () {
     if (localStorage == null) {
         alert('Your device doesn\'t support localStorage.')
@@ -23,7 +41,23 @@ window.addEventListener('load', async function () {
     /** @type {ProfileData} */
     const profile = getCache(STORAGE_PROFILE);
 
+    /** @type {TransactionsListResult} */
     const transactionsResult = await get('/user/transactions', token);
-    const transactions = transactionsResult.data;
+    const transactions = transactionsResult.data.transactions;
     console.info('Transactions:', transactions);
+
+    const transactionsList = _('transactionsList');
+    for (const transaction of transactions) {
+        const row = document.createElement('tr');
+        row.innerHTML = `<td>${transaction.date}</td>` +
+            `<td>${transaction.type.type}</td>` +
+            `<td>${transaction.description}</td>` +
+            `<td>${transaction.units}</td>` +
+            `<td>${transaction.pricePerUnit}</td>` +
+            `<td>${transaction.units * transaction.pricePerUnit}</td>`;
+        transactionsList.append(row);
+    }
+    if (transactions.length <= 0) {
+        _('transactionsEmpty').style.display = 'block';
+    }
 });
