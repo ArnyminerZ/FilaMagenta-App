d53300b9f2972f6b124623f6d0357d62976cde48
Arnau Mora <arnyminerz@proton.me>
2023-12-13 22:57:20 +0100

    Added error name
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- server/src/main/kotlin/com/filamagenta/response/FailureResponse.kt
+++ server/src/main/kotlin/com/filamagenta/response/FailureResponse.kt
@@ -13,11 +13,13 @@ data class FailureResponse(
     data class Error(
         val code: Int = -1,
         val message: String? = null,
+        val type: String? = null,
         val stackTrace: List<String>? = null
     ) {
         constructor(throwable: Throwable, code: Int = -1) : this(
             code,
             throwable.message,
+            throwable::class.simpleName,
             throwable.stackTrace.map { it.toString() }
         )
     }
