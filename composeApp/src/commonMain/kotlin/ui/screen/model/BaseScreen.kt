c5661e6c7da4cf4b8e9ae378e13b1c9244334266
Arnau Mora <arnyminerz@proton.me>
2023-12-14 18:44:07 +0100

    Ignore
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/commonMain/kotlin/ui/screen/model/BaseScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/screen/model/BaseScreen.kt
@@ -1,8 +1,10 @@
 package ui.screen.model
 
+import KoverIgnore
 import cafe.adriel.voyager.core.screen.Screen
 
 /**
  * Screens inheriting from this class won't support going back.
  */
+@KoverIgnore
 interface BaseScreen : Screen
