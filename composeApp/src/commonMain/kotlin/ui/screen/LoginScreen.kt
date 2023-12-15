cb34a7ca14bc66cfe57c05a79058fa199c2fed3f
Arnau Mora <arnyminerz@proton.me>
2023-12-15 16:16:44 +0100

    Cleanup
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/commonMain/kotlin/ui/screen/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/screen/LoginScreen.kt
@@ -3,6 +3,7 @@ package ui.screen
 import accounts.Account
 import accounts.AccountManager
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -52,8 +53,6 @@ object LoginScreen : BaseScreen() {
     override fun ScreenContent(paddingValues: PaddingValues) {
         val navigator = LocalNavigator.current
 
-        val isLoading by isLoading.collectAsState(false)
-
         AccountsHandler { accounts ->
             if (accounts.isNotEmpty()) {
                 navigator?.push(MainScreen)
@@ -66,74 +65,85 @@ object LoginScreen : BaseScreen() {
                 .padding(top = 16.dp)
                 .padding(horizontal = 8.dp)
         ) {
-            Text(
-                text = stringResource(MR.strings.login_title),
-                style = MaterialTheme.typography.headlineLarge,
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center
-            )
-            Text(
-                text = stringResource(MR.strings.login_subtitle),
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center
-            )
+            Titles()
+            LoginForm()
+        }
+    }
 
-            var nif by remember { mutableStateOf("") }
-            var password by remember { mutableStateOf("") }
+    @Composable
+    fun Titles() {
+        Text(
+            text = stringResource(MR.strings.login_title),
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = stringResource(MR.strings.login_subtitle),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+    }
 
-            val isNifValid = nif.isBlank() || nif.isValidNif
+    @Composable
+    fun ColumnScope.LoginForm() {
+        val isLoading by isLoading.collectAsState(false)
 
-            val passwordFocusRequester = remember { FocusRequester() }
+        var nif by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
 
-            FormField(
-                value = nif,
-                onValueChange = { nif = it.uppercase() },
-                label = stringResource(MR.strings.login_nif),
-                enabled = !isLoading,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-                    .padding(top = 8.dp)
-                    .autofill(listOf(AutofillType.Username)) { nif = it },
-                nextFocusRequester = passwordFocusRequester,
-                onSubmit = { login(nif, password) },
-                error = stringResource(MR.strings.login_error_nif).takeIf { !isNifValid },
-                capitalization = KeyboardCapitalization.Characters,
-                supportingText = stringResource(MR.strings.login_nif_info)
-            )
-            FormField(
-                value = password,
-                onValueChange = { password = it },
-                label = stringResource(MR.strings.login_password),
-                enabled = !isLoading,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-                    .padding(top = 8.dp)
-                    .focusRequester(passwordFocusRequester)
-                    .autofill(listOf(AutofillType.Password)) { nif = it },
-                isPassword = true,
-                onSubmit = { login(nif, password) }
-            )
+        val isNifValid = nif.isBlank() || nif.isValidNif
 
-            OutlinedButton(
-                onClick = { login(nif, password) },
-                enabled = !isLoading && isNifValid && nif.isNotBlank() && password.isNotBlank(),
-                modifier = Modifier.padding(end = 16.dp).align(Alignment.End)
+        val passwordFocusRequester = remember { FocusRequester() }
+
+        FormField(
+            value = nif,
+            onValueChange = { nif = it.uppercase() },
+            label = stringResource(MR.strings.login_nif),
+            enabled = !isLoading,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .padding(top = 8.dp)
+                .autofill(listOf(AutofillType.Username)) { nif = it },
+            nextFocusRequester = passwordFocusRequester,
+            onSubmit = { login(nif, password) },
+            error = stringResource(MR.strings.login_error_nif).takeIf { !isNifValid },
+            capitalization = KeyboardCapitalization.Characters,
+            supportingText = stringResource(MR.strings.login_nif_info)
+        )
+        FormField(
+            value = password,
+            onValueChange = { password = it },
+            label = stringResource(MR.strings.login_password),
+            enabled = !isLoading,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .padding(top = 8.dp)
+                .focusRequester(passwordFocusRequester)
+                .autofill(listOf(AutofillType.Password)) { nif = it },
+            isPassword = true,
+            onSubmit = { login(nif, password) }
+        )
+
+        OutlinedButton(
+            onClick = { login(nif, password) },
+            enabled = !isLoading && isNifValid && nif.isNotBlank() && password.isNotBlank(),
+            modifier = Modifier.padding(end = 16.dp).align(Alignment.End)
+        ) {
+            AnimatedVisibility(
+                visible = isLoading,
+                modifier = Modifier.padding(end = 8.dp)
             ) {
-                AnimatedVisibility(
-                    visible = isLoading,
-                    modifier = Modifier.padding(end = 8.dp)
-                ) {
-                    CircularProgressIndicator(
-                        modifier = Modifier.size(16.dp),
-                        strokeWidth = 2.dp
-                    )
-                }
-
-                Text(stringResource(MR.strings.login_action))
+                CircularProgressIndicator(
+                    modifier = Modifier.size(16.dp),
+                    strokeWidth = 2.dp
+                )
             }
+
+            Text(stringResource(MR.strings.login_action))
         }
     }
 
@@ -141,7 +151,7 @@ object LoginScreen : BaseScreen() {
         try {
             isLoading.tryEmit(true)
 
-            Napier.i { "Logging in as ${nif}..." }
+            Napier.i { "Logging in as $nif..." }
             val token = Authentication.login(nif, password)
             Napier.i { "Logged in successfully, adding account..." }
             val account = Account(nif)
@@ -156,6 +166,7 @@ object LoginScreen : BaseScreen() {
                 ErrorCodes.Authentication.Login.USER_NOT_FOUND -> {
                     snackbarError.tryEmit(MR.strings.login_error_not_found)
                 }
+
                 ErrorCodes.Authentication.Login.WRONG_PASSWORD -> {
                     snackbarError.tryEmit(MR.strings.login_error_wrong_password)
                 }
