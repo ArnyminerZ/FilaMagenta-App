7cb487f4b58fe4d2f61c57e61497b637ec6f426c
Arnau Mora <arnyminerz@proton.me>
2023-12-15 09:48:22 +0100

    Added icons extended
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -49,6 +49,7 @@ kotlin {
                 implementation(compose.runtime)
                 implementation(compose.foundation)
                 implementation(compose.material3)
+                implementation(compose.materialIconsExtended)
 
                 // Compose - Resources
                 api(libs.moko.resources)
