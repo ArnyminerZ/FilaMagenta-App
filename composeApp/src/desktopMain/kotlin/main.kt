1155dcc1cbcc0b6802757ace7f697f28b421b971
Arnau Mora <arnyminerz@proton.me>
2023-12-16 13:29:55 +0100

    Increased initial window size
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -2,8 +2,11 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -17,8 +20,13 @@ fun main() {
     application {
         val title by windowTitle.collectAsState()
 
+        val state = rememberWindowState(
+            size = DpSize(1000.dp, 700.dp)
+        )
+
         Window(
             title = title?.let { "Filà Magenta - $it" } ?: "Filà Magenta",
+            state = state,
             onCloseRequest = ::exitApplication
         ) {
             AppTheme {
